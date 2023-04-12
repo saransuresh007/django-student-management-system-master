a6cea77887da8b1b8e9984b7b91de3e53998c761
saran KS <sks@presidio.com>
2023-04-12 21:00:30 +0530

    pylint check2

--- student_management_app/EmailBackEnd.py
+++ student_management_app/EmailBackEnd.py
@@ -10,7 +10,6 @@ class EmailBackEnd(ModelBackend):
             user = UserModel.objects.get(email=username)
         except UserModel.DoesNotExist:
             return None
-        else:
-            if user.check_password(password):
-                return user
+        if user.check_password(password):
+            return user
         return None
